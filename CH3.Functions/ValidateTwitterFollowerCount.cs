bb735c62f452a5b372b7e6f9effc09ceb501f0a1
Prashant Sharma <prashant.r.sharma@capgemini.com>
2018-08-25 01:50:06 +0530

    Added Files to Ch3

---
+++ CH3.Functions/ValidateTwitterFollowerCount.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using SendGrid.Helpers.Mail;
+
+namespace CH3.Functions
+{
+    public static class ValidateTwitterFollowerCount
+    {
+        [FunctionName("ValidateTwitterFollowerCount")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req,
+            [SendGrid()] IAsyncCollector<Mail> message,
+            TraceWriter log)
+        {
+            try
+            {
+                log.Info("ValidateTwitterFollowerCount function processed a request.");
+
+                var tweetData = new
+                {
+                    TweetText = "",
+                    FollowersCount = "",
+                    Name = ""
+
+                };
+
+                var tweet = JsonConvert.DeserializeAnonymousType(await req.Content.ReadAsStringAsync(), tweetData);
+
+                if(Convert.ToInt32(tweet.FollowersCount) > 100)
+                {
+                    Mail mail = new Mail()
+                    {
+                        Subject = $"Thanks {tweet.Name} with followers {tweet.FollowersCount} has posted a tweet!"
+
+                    };
+
+                    Content content = new Content
+                    {
+                        Type = "text/plain",
+                        Value = $" Tweet Content : {tweetData.TweetText}"
+                    };
+
+                    var personalization = new Personalization();
+                    personalization.AddTo(new Email(System.Environment.GetEnvironmentVariable("EmailTo")));
+
+                    await message.AddAsync(mail);
+
+                }
+                //else
+                //{
+                //    message = null;
+                //}
+
+                return req.CreateResponse(HttpStatusCode.OK);
+
+            }
+            catch (System.Exception ex)
+            {
+                return req.CreateResponse(HttpStatusCode.InternalServerError, $"We apologize but something went wrong on our end.Exception:{ex.Message}");
+            }
+            finally
+            {
+                log.Info("ValidateTwitterFollowerCount function has finished processing a request.");
+            }
+           
+        }
+    }
+}
